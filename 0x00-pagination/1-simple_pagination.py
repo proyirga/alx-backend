51b59e4a6a97501c46845cc8373b9f578083822a
Yirga Mulaw <infoyirga12@gmail.com>
2023-10-24 08:19:15 +0300

    simple pagination

--- 0x00-pagination/1-simple_pagination.py
+++ 0x00-pagination/1-simple_pagination.py
@@ -7,6 +7,21 @@ import math
 from typing import List
 
 
+def index_range(page: int, page_size: int) -> tuple:
+    """
+    Args:
+    page: int
+    page_size: int
+
+    Returns:
+    tuple
+    """
+    start_index = (page - 1) * page_size
+    end_index = page * page_size
+
+    return start_index, end_index
+
+
 class Server:
     """Server class to paginate a database of popular baby names.
     """
@@ -25,12 +40,6 @@ class Server:
             self.__dataset = dataset[1:]
 
         return self.__dataset
-
-    def index_range(page: int, page_size: int) -> tuple:
-        """tuple of start and end ranges"""
-        start_index = (page - 1) * page_size
-        end_index = start_index + page_size
-        return (start_index, end_index)
 
 
     def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
