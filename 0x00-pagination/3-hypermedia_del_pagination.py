f245746da943cd72dd2f94aa811946a6ebdefd75
Yirga Mulaw <infoyirga12@gmail.com>
2024-01-04 05:27:30 +0000

    Index range

--- 0x00-pagination/3-hypermedia_del_pagination.py
+++ 0x00-pagination/3-hypermedia_del_pagination.py
@@ -17,7 +17,7 @@ def index_range(page: int, page_size: int) -> tuple:
     tuple
     """
     start_index = (page - 1) * page_size
-    end_index = page * page_size
+    end_index = page + page_size
     
     return start_index, end_index
 
