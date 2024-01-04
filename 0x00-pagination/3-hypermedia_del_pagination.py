9b9fa8f7ad0b225e788046327f8caee5a28e9ed6
Yirga Mulaw <infoyirga12@gmail.com>
2024-01-04 05:32:35 +0000

    Trailing white spaces

--- 0x00-pagination/3-hypermedia_del_pagination.py
+++ 0x00-pagination/3-hypermedia_del_pagination.py
@@ -12,13 +12,13 @@ def index_range(page: int, page_size: int) -> tuple:
     Args:
     page: int
     page_size: int
-    
+
     Returns:
     tuple
     """
     start_index = (page - 1) * page_size
     end_index = page + page_size
-    
+
     return start_index, end_index
 
 
@@ -47,16 +47,16 @@ class Server:
         Args:
         page: int
         page_size: int
-        
+
         Returns:
         List[List]
         """
         assert isinstance(page, int) and isinstance(page_size, int), "Both arguments must be integers."
         assert page > 0 and page_size > 0, "Both arguments must be greater than 0."
-        
+
         start_index, end_index = index_range(page, page_size)
         dataset = self.dataset()
-        
+
         return dataset[start_index:end_index]
 
 
@@ -65,16 +65,16 @@ class Server:
         Args:
         page: int
         page_size: int
-        
+
         Returns:
         dict
         """
         assert isinstance(page, int) and isinstance(page_size, int), "Both arguments must be integers."
         assert page > 0 and page_size > 0, "Both arguments must be greater than 0."
-        
+
         dataset = self.get_page(page, page_size)
         total_pages = math.ceil(len(self.dataset()) / page_size)
-        
+
         return {
             "page_size": len(dataset),
             "page": page,
